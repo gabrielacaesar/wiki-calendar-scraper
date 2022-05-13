f9c58e5b012071d0e5c085daa5659e7e447ad41f
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-05-13 18:27:31 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask, request
+from calender_scraper import get_events
+
+# coisas do site
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+	arquivo = open("templates/home.html")
+	return arquivo.read()
