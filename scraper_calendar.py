63118c995db132d135e90f6f0f9579287ffa9771
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-05-14 11:52:59 -0300

    Update scraper_calendar.py

--- scraper_calendar.py
+++ scraper_calendar.py
@@ -10,12 +10,13 @@ def get_events(user_input_month, user_input_lang):
 
   filtered_base_links = base_links.loc[filter]
   date_range = filtered_base_links['url'].tolist()
-
+  agendas = []
+  date_agendas = []
+  
   for date in date_range:
     page = requests.get(date)
     doc = lxml.html.fromstring(page.content)
-    agendas = []
-    date_agendas = []
+    
     if user_input_lang == 'pt':
       eventos = doc.xpath('//span[@id="Feriados_e_eventos_c.C3.ADclicos"]/parent::h2/following-sibling::ul/li')
     elif user_input_lang == 'en':
@@ -26,11 +27,14 @@ def get_events(user_input_month, user_input_lang):
       eventos = doc.xpath('//span[@id="Celebraciones"]/parent::h2/following-sibling::ul/li')
     else:
       print("Work in progress")  
+      
     for evento in eventos:
       agenda = evento.text_content()
       agendas.append(agenda)
       date_agendas.append(date)
+      
     dic_events = {'event': agendas,
                 'url': date_agendas}
     df_events = pd.DataFrame(dic_events)
+    
   return df_events
