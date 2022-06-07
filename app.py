c39c885b47ba5acb9576e7f2208edc03ebacfead
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-07 16:33:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	arquivo = open("templates/home.html")
+	
 	return arquivo.read()
+
+@app.route("/en")
+def display_en():
+	events = get_events("maio", "en")
+	return f"""
+	<p>{events}</p>
+	"""
 
 @app.route("/calendar")
 def display_events():
 	events = get_events("maio", "pt")
 	return f"""
-	<p>{events}</p>
+	<tr>
+	<td>{events[0]}</td>
+	</tr>
 	"""
