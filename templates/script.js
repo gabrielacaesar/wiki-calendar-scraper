a062cc1f050d72b8fd11efda36cb9ce4becdab9c
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-10 18:42:08 -0300

    add second try

--- templates/script.js
+++ templates/script.js
@@ -24,3 +24,30 @@ function definirOutput(optionMonth, optionLanguage){
     }
   }
 }
+
+
+// tentativa dois 
+// com conteudo do video do vinicius
+function definirOutput(optionMonth, optionLanguage){
+
+  // dados com endpoint
+  let url = 'https://wiki-calendar-scraper.herokuapp.com/events?month=June&lang=en'
+
+  // esperar carregar para o console.log
+  fetch(url).then(
+    function(resposta){
+      console.log(resposta.json())
+    }
+  )
+
+  // para cada linha do json vamos...
+  for (let row of json) {
+    console.log(row)  
+    // considera o que o usuario colocou de mes e linguage
+    if (row.month == optionMonth.value && row.language == optionLanguage.value){
+      // retorna o df considerando condicao
+      let result = df_events
+      break
+    }
+  }
+}
