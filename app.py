7274bbd468628e4bd33b8a828ff871dadfcba2c3
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-09 21:56:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify, after_this_request
 from scraper_calendar import get_events
+import flask, json
 
 # coisas do site
 app = Flask(__name__)
@@ -9,6 +10,24 @@ app = Flask(__name__)
 def hello_world():
 	arquivo = open("templates/home.html")
 	return arquivo.read()
+
+@app.route("/eventos", methods=['GET'])
+def get_content():
+    @after_this_request
+    def add_header(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+        
+    month = str(request.args['month'])
+    lang = str(request.args['lang'])
+
+    try:
+        output = get_events(month, lang)
+
+    except ValueError as e:
+        return {"error":1}
+
+    return jsonify(output)
 
 @app.route("/en-june")
 def display_en():
