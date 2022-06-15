dc52c05995ddabdd07e68b585da3f38ba19d55dd
tiagombp <tiagombp@gmail.com>
2022-06-15 13:32:38 -0300

    faz pequenos ajustes e adapta fetch para buscar arquivos locais

--- templates/script.js
+++ templates/script.js
@@ -3,27 +3,48 @@
 let optionLanguage = document.querySelector('#language')
 let optionMonth = document.querySelector('#month')
 
-console.log(optionLanguage.target.value)
-console.log(optionMonth)
+console.log(optionLanguage.value);
+console.log(optionMonth.value);
 
 // executa funcao considerando variavel
 optionLanguage.addEventListener('change', definirOutput)
 optionMonth.addEventListener('change', definirOutput)
 
 // fetch
-function definirOutput(optionMonth, optionLanguage){
+function definirOutput() {
 
   console.log(optionLanguage.value)
   console.log(optionMonth.value)
   // dados com endpoint
-  let url = 'https://raw.githubusercontent.com/gabrielacaesar/wiki-calendar-scraper/main/data/' + optionLanguage.target.value + "-" + optionMonth.target.value + '-content.json?token=GHSAT0AAAAAABTCSP6ODVDWSZ63YZX6H2YUYVI4KZA'
+  //let url = 'https://raw.githubusercontent.com/gabrielacaesar/wiki-calendar-scraper/main/data/' + optionLanguage.value + "-" + optionMonth.value + '-content.json?token=GHSAT0AAAAAABTCSP6ODVDWSZ63YZX6H2YUYVI4KZA'
+
+  // dados locais
+
+  let url;
+
+  if (optionLanguage.value == 'pt') {
+
+    url = `../data/pt-${optionMonth.value}-content.json`;
+
+  } else {
+
+    console.log('Só temos dados em português, sorry gringos.');
+    return
+
+  }
 
   console.log(url)
   // esperar carregar para o console.log
   fetch(url).then(
     function(resposta){
-      console.log(resposta.json())
+      return resposta.json()
+    }
+  ).then(
+    function(data) {
+      console.log(data);
     }
   )
 
 }
+
+
