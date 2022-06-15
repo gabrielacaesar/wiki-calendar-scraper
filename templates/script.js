3168c75e2c698852b4968ed100dc9da8f92ee73a
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-15 15:06:24 -0300

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -1,10 +1,6 @@
 // variavel
-
 let optionLanguage = document.querySelector('#language')
 let optionMonth = document.querySelector('#month')
-
-console.log(optionLanguage.value);
-console.log(optionMonth.value);
 
 // executa funcao considerando variavel
 optionLanguage.addEventListener('change', definirOutput)
@@ -13,27 +9,9 @@ optionMonth.addEventListener('change', definirOutput)
 // fetch
 function definirOutput() {
 
-  console.log(optionLanguage.value)
-  console.log(optionMonth.value)
-  // dados com endpoint
-  //let url = 'https://raw.githubusercontent.com/gabrielacaesar/wiki-calendar-scraper/main/data/' + optionLanguage.value + "-" + optionMonth.value + '-content.json?token=GHSAT0AAAAAABTCSP6ODVDWSZ63YZX6H2YUYVI4KZA'
-
-  // dados locais
-
-  let url;
-
-  if (optionLanguage.value == 'pt') {
-
-    url = `../data/pt-${optionMonth.value}-content.json`;
-
-  } else {
-
-    console.log('Só temos dados em português, sorry gringos.');
-    return
-
-  }
-
+  let url = `../data/${optionLanguage.value}-${optionMonth.value}-content.json`;
   console.log(url)
+
   // esperar carregar para o console.log
   fetch(url).then(
     function(resposta){
@@ -42,15 +20,16 @@ function definirOutput() {
   ).then(
     function(data) {
       let html = '';
+      // insere dados em cada linha com o respectivo link
+      // adiciona cada linha ao html
       for (let row of data){
         let efemeride = '<li><a href="' + row.url + '">' + row.event + '</a></li>';
         html += efemeride
       }
+      // insere dentro do ul no html
       document.querySelector('ul').innerHTML = html
       console.log(data);
     }
   )
 
 }
-
-
