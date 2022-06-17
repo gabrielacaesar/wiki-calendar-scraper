49ffc7d112f8f7e198ab917ac93261a126a1a27b
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-06-17 16:08:10 -0300

    list to dic + json output name + clean name + new languages

--- scraper_calendar.py
+++ scraper_calendar.py
@@ -6,10 +6,13 @@ import json
 
 base_links = pd.read_csv("base-links_calendar-trends.csv")
 
-month_en = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-month_de = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"]
-month_pt = ["janeiro", "fevereiro", "mar%C3%A7o", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-month_es = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+dic_lang = {'en': ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            'de': ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+            'pt': ["janeiro", "fevereiro", "mar%C3%A7o", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"],
+            'es': ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"],
+            'fr': ["janvier", "f%C3%A9vrier", "mars", "avril", "mai", "juin", "juillet", "ao%C3%BBt", "septembre", "octobre", "novembre", "d%C3%A9cembre"],
+            'it': ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"],
+            'nl': ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"]}
 
 def get_events(user_input_month, user_input_lang):
 
@@ -17,6 +20,7 @@ def get_events(user_input_month, user_input_lang):
 
   filtered_base_links = base_links.loc[filter]
   url_list = filtered_base_links['url'].tolist()
+  #print(url_list)
   agendas = []
   url_agendas = []
   
@@ -32,8 +36,14 @@ def get_events(user_input_month, user_input_lang):
       eventos = doc.xpath('//span[@id="Feier-_und_Gedenktage"]/parent::h2/following-sibling::ul/li')
     elif user_input_lang == 'es':
       eventos = doc.xpath('//span[@id="Celebraciones"]/parent::h2/following-sibling::ul/li')
+    elif user_input_lang == 'fr':
+      eventos = doc.xpath('//span[@id="C.C3.A9l.C3.A9brations"]/parent::h2/following-sibling::ul/li')
+    elif user_input_lang == 'it':
+      eventos = doc.xpath('//span[@id="Feste_e_ricorrenze"]/parent::h2/following-sibling::ul/li')
+    elif user_input_lang == 'nl':
+      eventos = doc.xpath('//span[@id="Viering.2Fherdenking"]/parent::h2/following-sibling::ul/li')
     else:
-      print("Work in progress")  
+      print("Failed!")  
       
     for evento in eventos:
       agenda = evento.text_content()
@@ -45,25 +55,25 @@ def get_events(user_input_month, user_input_lang):
     df_events = pd.DataFrame(dic_events)
 
     df_events['date'] = df_events['url'].str.replace(f'http://{user_input_lang}.wikipedia.org/wiki/', '')
-
     if user_input_lang in ['en', 'de']:
-      df_events['date'] = df_events['date'].str.replace('_', ' ')
-      df_events[['month', 'day']] = df_events['date'].str.split(expand = True)
+      df_events['date_clean'] = df_events['date'].str.replace('_', ' ')
+      df_events[['month', 'day']] = df_events['date_clean'].str.split(expand = True)
     elif user_input_lang in ['pt', 'es']:
-      df_events['date'] = df_events['date'].str.replace('_de_', ' ')
-      df_events[['day', 'month']] = df_events['date'].str.split(expand = True)
+      df_events['date_clean'] = df_events['date'].str.replace('_de_', ' ')
+      df_events[['day', 'month']] = df_events['date_clean'].str.split(expand = True)
+      df_events['date'] = df_events['date'].str.replace('_', ' ')
     else:
       print('other lang')
-
-    df_events['month_id'] = month_es.index(f"{user_input_month}") + 1 # change language object; 'es' as default
+      
+    month_id = dic_lang[f"{user_input_lang}"]
+    df_events['month_id'] = month_id.index(f"{user_input_month}") + 1
 
     df_events['day'] = df_events['day'].str.pad(width=2, fillchar='0')
     df_events['month_id'] = df_events['month_id'].astype(str).str.pad(width=2, fillchar='0')
     df_events['format_date'] = df_events['month_id'].astype(str) + "/" + df_events['day'].astype(str)
-    df_events.drop(['month', 'day'], inplace=True, axis=1) 
+    df_events.drop(['month', 'day', 'date'], inplace=True, axis=1) 
 
     #arquivo = df_events.to_csv(f"content_{user_input_lang}_{user_input_month}.csv", encoding='utf-8', index=False)
-    json_events = df_events.to_json(f"{user_input_lang}-{user_input_month}-content.json", orient="index", date_format="iso")
-    #content_pt_01 = json.dumps(json_events)
+    json_events = df_events.to_json(f"{user_input_lang}-{df_events['month_id'].values[0]}-content.json", orient="records", date_format="iso")
     
   return json_events
